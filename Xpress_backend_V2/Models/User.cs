187544603988549ad06acfe94b634f7829e36757
Gautham Krishna N <gkn240801@gmail.com>
2025-06-04 10:12:27 +0530

    Phone number field to the user table

--- Xpress_backend_V2/Models/User.cs
+++ Xpress_backend_V2/Models/User.cs
@@ -4,6 +4,7 @@
     {
         public int UserId { get; set; } // PK
         public string EmployeeName { get; set; }
+        public string PhoneNumber { get; set; } 
         public string UserRole { get; set; }
         public string Department { get; set; }
         public bool IsActive { get; set; }
