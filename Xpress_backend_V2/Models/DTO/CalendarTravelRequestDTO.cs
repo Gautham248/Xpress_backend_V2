86e99c23c6d32b851c641b53a82ce4c7564e5548
staticclown <george38691@gmail.com>
2025-06-05 00:20:21 +0530

    added the calendar

---
+++ Xpress_backend_V2/Models/DTO/CalendarTravelRequestDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Xpress_backend_V2.Models.DTO
+{
+    public class CalendarTravelRequestDTO
+    {
+        public string RequestId { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime OutboundDepartureDate { get; set; }
+        public DateTime OutboundArrivalDate { get; set; }
+        public DateTime? ReturnDepartureDate { get; set; }
+        public DateTime? ReturnArrivalDate { get; set; }
+        public string SourcePlace { get; set; }
+        public string SourceCountry { get; set; }
+        public string DestinationPlace { get; set; }
+        public string DestinationCountry { get; set; }
+        public string CurrentStatusName { get; set; }
+    }
+}
+
