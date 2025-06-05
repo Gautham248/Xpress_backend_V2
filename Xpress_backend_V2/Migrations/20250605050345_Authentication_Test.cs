4f19042bc6f54eeda7ed664db0cb1a07e6c4b4b0
Gautham Krishna N <gkn240801@gmail.com>
2025-06-05 10:49:37 +0530

    Authentication Added

---
+++ Xpress_backend_V2/Migrations/20250605050345_Authentication_Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Xpress_backend_V2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Authentication_Test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
