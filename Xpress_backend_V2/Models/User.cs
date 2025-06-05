1c720f9a81f5b9bbc26480768157747ccdfd0f3c
Gautham Krishna N <gkn240801@gmail.com>
2025-06-05 10:15:02 +0530

    Modified User Table

--- Xpress_backend_V2/Models/User.cs
+++ Xpress_backend_V2/Models/User.cs
@@ -4,6 +4,8 @@
     {
         public int UserId { get; set; } // PK
         public string EmployeeName { get; set; }
+        public string EmployeeEmail { get; set; }
+        public string Password { get; set; }
         public string PhoneNumber { get; set; } 
         public string UserRole { get; set; }
         public string Department { get; set; }
