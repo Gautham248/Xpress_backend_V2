f3f87962fc378b11786c4f02e1069425de60c97b
staticclown <george38691@gmail.com>
2025-06-08 19:54:05 +0530

    update

--- Xpress_backend_V2/Program.cs
+++ Xpress_backend_V2/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.OpenApi.Models;
 using System.Text;
 using Xpress_backend_V2.Data;
 using Xpress_backend_V2.Interface;
+using Xpress_backend_V2.Repositories;
+
 //using Xpress_backend_V2.Repositories;
 using Xpress_backend_V2.Repository;
 using Xpress_backend_V2.Services;
@@ -42,6 +44,9 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ITravelRequestStatsRepository, TravelRequestStatsRepository>();
 builder.Services.AddScoped<IDocumentService, DocumentRepository>();
 builder.Services.AddScoped<IDashboardRepository, DashboardRepository>();
+builder.Services.AddScoped<IProcessingTimeRepository, ProcessingTimeRepository>();
+builder.Services.AddScoped<IDocumentStatusRepository, DocumentStatusRepository>();
+builder.Services.AddScoped<ITravelAgencyStatRepository, TravelAgencyStatRepository>();
 
 builder.Services.AddAutoMapper(typeof(Program));
 
