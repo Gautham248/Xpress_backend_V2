f745f12804b9240e8ce0f2447a7fb70f5a27e25b
RionaMariaVarghese <rionamariav@gmail.com>
2025-07-02 11:44:23 +0530

    file docs

--- Xpress_backend_V2/Models/TravelRequest.cs
+++ Xpress_backend_V2/Models/TravelRequest.cs
@@ -37,7 +37,7 @@ namespace Xpress_backend_V2.Models
         public decimal? TotalExpense { get; set; }
         public List<string>? TicketDocumentPath { get; set; }
         [Column(TypeName = "jsonb")]
-        public List<string>? AccomodationDocumentPath { get; set; }
+        public List<string>? AccommodationDocumentPath { get; set; }
         [Column(TypeName = "jsonb")]
         public List<string>? InsuranceDocumentPath { get; set; }
         public string? LDCertificatePath { get; set; }
